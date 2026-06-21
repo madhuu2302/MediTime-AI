a483f3da8e860bda1f9b9b144ba5a245084f3833
Madhumitha <madhuu2317@gmail.com>
2026-06-21 20:13:09 +0530

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 import streamlit as st
-
 st.title("💊 MediTime AI")
-
 name = st.text_input("Patient Name")
 medicine = st.text_input("Medicine Name")
-
 time = st.selectbox(
     "Medicine Time",
     ["Morning", "Afternoon", "Night"]
 )
-
 if st.button("Set Reminder"):
     st.success(f"Reminder set for {medicine} at {time}")
-
 st.subheader("Medicine Status")
-
 taken = st.radio(
     "Did you take your medicine?",
     ["Yes", "No"]
 )
-
 if taken == "Yes":
     st.success("Great! Keep following your schedule.")
 else:
