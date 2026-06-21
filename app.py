f0f9b12c8d69e7db9e5c63fa819867a2467e7b9b
Madhumitha <madhuu2317@gmail.com>
2026-06-21 20:12:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("💊 MediTime AI")
+
+name = st.text_input("Patient Name")
+medicine = st.text_input("Medicine Name")
+
+time = st.selectbox(
+    "Medicine Time",
+    ["Morning", "Afternoon", "Night"]
+)
+
+if st.button("Set Reminder"):
+    st.success(f"Reminder set for {medicine} at {time}")
+
+st.subheader("Medicine Status")
+
+taken = st.radio(
+    "Did you take your medicine?",
+    ["Yes", "No"]
+)
+
+if taken == "Yes":
+    st.success("Great! Keep following your schedule.")
+else:
+    st.warning("Please take your medicine on time.")
